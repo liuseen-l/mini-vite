3212a2b110a17f77dbb42e1f35accf004a68c184
liuseen-l <773890440@qq.com>
2023-07-20 01:30:23 +0800

    feat: resolve

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-
 // @ts-expect-error 必须要用后置，因为 server 没做自动加后缀的兼容
 import { subModule } from './sub-module.ts'
 
-// import './style/style.css'
+// @ts-expect-error
+import { ReactComponent } from './react-component.tsx'
+import './style/style.css'
+
 // import './style/less-test.less'
 
-const config = defineConfig({})
 const app = document.getElementById('app')
-
 app!.innerText = 'Hello World'
 
 subModule(app!)
-// const comp = ReactComponent()
-// // @ts-expect-error
-// const root = ReactDOM.createRoot(document.getElementById('react-root'))
-// root.render(comp)
+const comp = ReactComponent()
+// @ts-expect-error
+const root = ReactDOM.createRoot(document.getElementById('react-root'))
+root.render(comp)
