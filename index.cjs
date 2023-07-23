a7811f1eb33a8ef0b3f23bbd309bae70f1767701
liuseen-l <773890440@qq.com>
2023-07-24 01:57:36 +0800

    feat: init

--- index.cjs
+++
@@ -1,7 +0,0 @@
-const path = require('node:path')
-
-function getShortName(file, root) {
-  return file.startsWith(`${root}/`) ? path.posix.relative(root, file) : file
-}
-
-console.log(getShortName('F:\forProjects\mini-vite\playground\src\main.ts', 'F:/forProjects/mini-vite/playground'))
